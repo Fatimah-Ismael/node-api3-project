72c3d80cfff35e2cf155ff0933e0d3e0d64bd583
Fatimah Ismael <fatimahismael@yahoo.com>
2023-03-29 12:28:52 -0700

    wip

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,62 @@
+const Users = require('../users/users-model')
+
 function logger(req, res, next) {
   // DO YOUR MAGIC
+  const timestamp = new Date().toLocaleString();
+  const method = req.method
+  const url = req.originalUrl
+  console.log(`[${timestamp} ${method} ${url}]`)
+  next()
 }
 
-function validateUserId(req, res, next) {
+async function validateUserId(req, res, next) {
   // DO YOUR MAGIC
+  try{
+    const user = await Users.getById(req.params.id)
+    if(!user){
+      res.status(404).json({
+        message:'no such user'
+      })
+    } else {
+      req.user = user
+      next()
+    }
+  } catch(err){
+    res.status(500).json({
+      message: 'Error finding user'
+    })
+  }
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
+  const {name}= req.body
+  if(!name){
+    res.status(400).json({
+      message: 'missing required name field'
+    })
+  } else {
+    next()
+  }
+  
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
+  const { text }= req.body;
+  if(!text){
+    res.status(400).json({
+      message: 'missing text required'
+    })
+  } else{
+    next()
+  }
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  logger, 
+  validatePost, 
+  validateUser,
+  validateUserId
+}
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const { validateUserId, 
+  validateUser,
+   validatePost } = require('../middleware/middleware')
 
 // You will need `users-model.js` and `posts-model.js` both
 // The middleware functions also need to be required
@@ -6,39 +9,56 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+  /*Users.get(req.query)
+  .then(users =>{
+  res.status(200).json(users)
+  })
+  .catch(error => {
+    // log error to server
+    console.log(error);
+    res.status(500).json({
+      message: 'Error getting users',
+    });
+  }); */
   // RETURN AN ARRAY WITH ALL THE USERS
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validateUserId, (req, res) => {
+  
+    console.log(req.user)
   // RETURN THE USER OBJECT
   // this needs a middleware to verify user id
 });
 
-router.post('/', (req, res) => {
+router.post('/', validateUser, (req, res) => {
   // RETURN THE NEWLY CREATED USER OBJECT
   // this needs a middleware to check that the request body is valid
+  console.log(req.user)
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', validateUserId, validateUser,(req, res) => {
   // RETURN THE FRESHLY UPDATED USER OBJECT
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', validateUserId,  (req, res) => {
   // RETURN THE FRESHLY DELETED USER OBJECT
   // this needs a middleware to verify user id
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/posts', validateUserId, (req, res) => {
   // RETURN THE ARRAY OF USER POSTS
   // this needs a middleware to verify user id
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts', validateUserId,validatePost, (req, res) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
+  console.log(req.user)
+  console.log(req.text)
 });
 
 // do not forget to export the router
+module.exports = router
